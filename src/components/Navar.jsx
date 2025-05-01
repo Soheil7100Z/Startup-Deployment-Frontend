23233726cd24f2e924c4c3c2b40316e13ba4af7e
Soheil Zaremehrjardi <soheil7100@gmail.com>
2025-05-01 15:23:42 +0200

    Startup website Project => added less and more icon for description of the items in products page and some edition in Navbar including adding menu option

--- src/components/Navar.jsx
+++ src/components/Navar.jsx
@@ -1,15 +1,52 @@
 import styles from './navar.module.css'
 import { NavLink } from 'react-router-dom'
 import {FaReact} from 'react-icons/fa'
+import {IoMdContact } from 'react-icons/io'
+import { useState } from 'react'
 
 const Navar = () => {
+
+    const [menu , menuActivating] = useState(true)
+
   return (
     <div id={styles.container}>
-        <NavLink to="/" className={styles.logo}><FaReact/></NavLink>
-        <div className={styles.title}>Start Up</div>
+        <div className={styles.leftSide} >
+
+            <div  onClick={() => menuActivating((preState) => !preState) }>
+                {menu ?
+                <div className={styles.menuSign}>
+                    <span></span><span></span><span></span>
+                </div>
+                    :
+                 <div>
+                        <div className={styles.menuSign}>
+                            <span></span><span></span><span></span>
+                        </div>
+                        <div className={styles.menu} onMouseLeave={() => menuActivating(true)}>
+                            <NavLink to="/">Home</NavLink><hr />
+                            <NavLink to="/Produkt">Usere Produkte</NavLink><hr />
+                            <NavLink to="/uber">Über uns</NavLink><hr />
+                            <NavLink to="/kontakt">Kontakt</NavLink>
+                        </div>
+                 </div>
+
+                }
+            </div>
+        </div>
+
+        <div className={styles.title}>
+        <div className={styles.logo}><FaReact/></div>
+            Start Up</div>
         <div className={styles.rightSide}>
-            <NavLink to="/uber" className={styles.uberUns}>Über Uns</NavLink>
-            <NavLink to="/kontakt" className={styles.uberUns}>Kontakt</NavLink>
+            <NavLink to="/uber" className={styles.uberUns}>
+
+            Über uns</NavLink>
+            <div className={styles.contactBox}>
+                <NavLink to="/kontakt" className={styles.uberUns}>
+                <IoMdContact className={styles.contactIcon}/>
+                Kontakt</NavLink>
+            </div>
+
         </div>
     </div>
   )
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react'
 import styles from './product.module.css'
 
 
 function Product({item}) {
+    let description = item.description
+    const [cutText, cuttingText] = useState(true)
+    if (cutText) {
+        description = description.substring(0,120) + '...'
+    }
+
+
   return (
 
         <div className={styles.card}>
@@ -9,7 +17,8 @@ function Product({item}) {
                 <div className={styles.img} style={{backgroundImage: `url(${item.img})`}}></div>
                 <div className={styles.inhalt}>
                     <div className={styles.title}>{item.title}</div>
-                    <div className={styles.description}> {item.description} </div>
+                    <div className={styles.description}> {description} </div>
+                    <button className={styles.moreLessButton} onClick={() => cuttingText((preState) => !preState)}> {cutText ? 'Mehr' : 'Weniger'} </button>
                     <div className={styles.price}>Preis: {item.price}</div>
                 </div>
             </div>
