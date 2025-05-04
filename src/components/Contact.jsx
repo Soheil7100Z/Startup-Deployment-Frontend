cb8a89db3c4b25b1c97357f35c2b3153b4008942
Soheil Zaremehrjardi <soheil7100@gmail.com>
2025-05-04 12:08:18 +0200

    Startup website Project => Edition on the contact page to make the submission work correctly

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,9 +4,6 @@ import {FaRegPaperPlane} from 'react-icons/fa'
 
 const Contact = () => {
 
-    let emailValidation = ''
-    const inputElements = document.querySelectorAll('input')
-
     const [inputVor , settingInputVor] = useState('')
     const [inputNach , settingInputNach] = useState('')
     const [inputEmail , settingInputEmail] = useState('')
@@ -63,46 +60,36 @@ const Contact = () => {
         }
     }
 
-    const validationChecker = () => {
-        if (!inputEmailRef.current.checkValidity()){
-            settingValidationEmail('NotValid')
-            emailValidation = 'NotValid'
-        }else{
-            settingValidationEmail('')
-            emailValidation = ''
+
+    const submitData = (e) => {
+        e.preventDefault()
+        if (!inputVor){
+            settingErrorVor('required')
+        }else {
+            settingErrorVor('')
         }
-    }
+        if (!inputNach){
+            settingErrorNach('required')
+        }else {
+            settingErrorNach('')
+        }
 
-    const submittingUser = () => {
-        inputElements.forEach(el =>{
-            if (inputVor===''){
-                settingErrorVor('required')
-            }else {
-                settingErrorVor('')
-            }
+        if (!inputEmail){
+            settingErrorEmail('required')
+        }else {
+            settingErrorEmail('')
+        }
 
-            if (inputNach===''){
-                settingErrorNach('required')
-            }else {
-                settingErrorNach('')
-            }
+        if (!inputText){
+            settingErrorText('required')
+        }else {
+            settingErrorText('')
+        }
 
-            if (inputEmail===''){
-                settingErrorEmail('required')
-            }else {
-                settingErrorEmail('')
-            }
-
-            if (inputText===''){
-                settingErrorText('required')
-            }else {
-                settingErrorText('')
-            }
-
-            if (inputVor!=='' && inputNach!=='' && inputEmail!=='' && inputText!=='' && emailValidation === ''){
-                let userData = []
-                let keys = ["Anrede","Vorname" , "Nachname" , "Email" , "Telefonnummer" , "Postleitzahl"]
-                userData.push(inputAnrede,inputVor,inputNach,inputEmail,inputTel,inputPost)
+        if (inputVor && inputNach && inputEmail && inputText){
+                let userData = [];
+                let keys = ["anrede","vorname" , "nachname" , "email" , "telefonnummer" , "postleitzahl", "message"]
+                userData.push(inputAnrede,inputVor,inputNach,inputEmail,inputTel,inputPost,inputText)
                 let userDataObject = Object.fromEntries(keys.map((key , i) => [key , userData[i]]))
                 settingInputVor('')
                 settingInputNach('')
@@ -110,21 +97,48 @@ const Contact = () => {
                 settingInputTel('')
                 settingInputPost('')
                 settingInputText('')
-                console.log(userDataObject)
-            }
+                console.log('submit' , userDataObject)
+                try {
+                    console.log('submission is working')}
+                catch (error) {
+                    console.error(error);
+                  }
+        }
+}
 
-        })
+    const Beforesubmission = () => {
+        if (!inputEmailRef.current.checkValidity()){
+            settingValidationEmail('NotValid')
+        }else{
+            settingValidationEmail('')
+        }
+        if (!inputVor){
+            settingErrorVor('required')
+        }else {
+            settingErrorVor('')
+        }
+        if (!inputNach){
+            settingErrorNach('required')
+        }else {
+            settingErrorNach('')
+        }
+
+        if (!inputText){
+            settingErrorText('required')
+        }else {
+            settingErrorText('')
+        }
     }
 
 
   return (
-    <form id={styles.container} >
+    <form id={styles.container} method="POST" action="send" onSubmit={submitData} >
         <span style={{display: 'block', marginBottom: '2rem' }}>Sie haben eine Frage? Dann schreiben Sie uns! Wir bemühen uns Ihr Anliegen zu bearbeiten und setzen uns schnellstmöglich mit Ihnen in Verbindung.</span>
         <span  style={{display: 'block', marginBottom: '2rem', fontWeight: 'bold', fontSize:'var(--text-lg)' }}>Persönliche Angaben</span>
         <div className={styles.form}>
             <div className={styles.perosnInfo}>
                 <label>Anrede:</label>
-                <select className={styles.selectionIcon} value={(inputAnrede)} onChange={currentInputAnrede}>
+                <select className={styles.selectionIcon} value={(inputAnrede)} onChange={currentInputAnrede} >
                         <option>Herr</option>
                         <option>Frau</option>
                         <option>Dr.</option>
@@ -134,33 +148,33 @@ const Contact = () => {
 
             <div className={styles.perosnInfo}>
                 <label style={{color: errorVor ? '#EA2027' : ''}}>Vorname*:</label>
-                <input type="text" value={inputVor} onChange={currentInputVor} style={{borderColor: errorVor ? '#EA2027' : ''}}/>
+                <input type="text" name="Vorname"  value={inputVor} onChange={currentInputVor}  style={{borderColor: errorVor ? '#EA2027' : ''}}/>
             </div>
 
             <div className={styles.perosnInfo}>
                 <label style={{color: errorNach ? '#EA2027' : ''}}>Nachname*:</label>
-                <input type="text" value={inputNach} onChange={currentInputNach} style={{borderColor: errorNach ? '#EA2027' : ''}}/>
+                <input type="text" name="Nachname"   value={inputNach} onChange={currentInputNach}  style={{borderColor: errorNach ? '#EA2027' : ''}}/>
             </div>
 
             <div className={styles.perosnInfo}>
                 <label style={{color: (errorEmail || errorValidationEmail) ? '#EA2027' : ''}}>E-Mail*:</label>
-                <input  type="Email" ref={inputEmailRef}
+                <input  type="Email" name="UserEmail"  ref={inputEmailRef}
                 value={inputEmail} onChange={currentInputEmail}
                 style={{borderColor: (errorEmail || errorValidationEmail) ? '#EA2027' : ''}}/>
             </div>
 
             <div className={styles.perosnInfo}>
                 <label>Telefonnummer:</label>
-                <input type="number" value={inputTel} onChange={currentInputTel}/>
+                <input type="number" name="Telefonnummer" value={inputTel} onChange={currentInputTel}/>
             </div>
 
             <div className={styles.perosnInfo}>
                 <label>Postleitzahl:</label>
-                <input type="number"  value={inputPost} onChange={currentInputPost} />
+                <input type="number" name="Postleitzahl" value={inputPost} onChange={currentInputPost} />
             </div>
         </div>
         {errorText && <div style={{marginBottom: '-3rem', marginTop: '2rem', color: '#EA2027', fontSize: '2rem' }}>*</div>}
-        <textarea className={styles.textArea} placeholder='Bitte schreiben Sie Ihre Anfrage'
+        <textarea name="message" rows="5" className={styles.textArea} placeholder='Bitte schreiben Sie Ihre Anfrage'
         value={inputText} onChange={currentInputText}
         style={{borderColor: errorText ? '#EA2027' : ''}}
           ></textarea>
@@ -168,7 +182,7 @@ const Contact = () => {
         {errorValidationEmail && <div style={{ color: '#EA2027', fontSize:'1.3rem'}}>Bitte geben Sie eine gültige E-Mail ein !</div>}
 
         {(errorVor||errorNach||errorEmail||errorText) && <div style={{color: '#EA2027', fontSize:'1.3rem', marginTop:'1rem'}}>Bitte füllen Sie die * aus, sie sind die Pflichtangaben !</div>}
-        <button type="button" onClick={() => {validationChecker(), submittingUser()}} className={styles.button}>
+        <button type="submit" onClick={Beforesubmission} className={styles.button}>
             <FaRegPaperPlane  style={{ marginRight: '.8rem'}} />
             SENDEN</button>
 
